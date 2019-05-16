f49017e0f4f6a11aed0ba43cfc0947af5e83732d
kevinche75 <47882198+kevinche75@users.noreply.github.com>
2019-05-16 19:01:15 +0300

    Delete NameWithNumbersException.java

--- src/NameWithNumbersException.java
+++
@@ -1,9 +0,0 @@
-import java.io.Serializable;
-
-public class NameWithNumbersException extends RuntimeException implements Serializable {
-    static final long serialVersionUID = 3;
-    NameWithNumbersException(){
-        System.out.println("NameWithNumbersException: Имя не должно содержать цифр");
-        System.exit(0);
-    }
-}
